a797debc823d950d307180b667e8fe5e64a3e118
Prashant1101-Jain <jainprashant718@gmail.com>
2023-12-06 01:34:08 +0530

    update button

---
+++ mathematica/src/Component/BarButton/Barbutton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BarButton = (Name) => {
+  <button type="button">
+    {Name.Name}
+    {console.log(Name.Name)}Name
+  </button>;
+};
+export default BarButton;
--- mathematica/src/Component/Header/Header.jsx
+++ mathematica/src/Component/Header/Header.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import './Header.styles.scss'
+import React from "react";
+import "./Header.styles.scss";
+import BarButton from "../BarButton/Barbutton";
 
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      content: [
+        {
+          Name: "PlayList",
+        },
+      ],
+    };
+  }
 
-const Header=()=>{
-    return(
-    
-           <div className = "header">
-             <div className='options'>
-                <div className = "option">
-                    Playlist
-                </div>
-                <div className = "option">
-                    Notes
-                </div>
-                <div className = "option">
-                    Previous Year Paper
-                </div>
-                <div className = "option" >
-                   Exams
-                </div>
-                <div className='option'>Mock Test</div>
-            </div></div>
-
-    )
+  render() {
+    return (
+      <div className="header">
+        <div className="options">
+          <div className="option">
+          {console.log(this.state.content)}
+            <BarButton Name={this.state.content[0].Name} />
+          </div>
+          <div className="option">Notes</div>
+          <div className="option">Previous Year Paper</div>
+          <div className="option">Exams</div>
+          <div className="option">Mock Test</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
